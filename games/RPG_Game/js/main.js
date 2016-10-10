71f67441dafb4a707236667ac6ad6399ab4b09e9
Emīls <emilsromanis@gmail.com>
2016-10-10 20:14:34 +0300

    Add files via upload

---
+++ games/RPG_Game/js/main.js
@@ -0,0 +1,387 @@
+var game = new Phaser.Game(1200, 624, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+var map_prop = {
+	width : 40,
+	height : 40,
+	tile_size : 48,
+	//(game_x/tile_size)%2==1 un (game_y/tile_size)%2==1
+	game_x : 1200,
+	game_y : 624,
+	field : 0,
+	cord_x : 0,
+	cord_y : 0,
+	world_size : 1,
+	min_map : true
+};
+
+var night;
+var turn;
+
+var boot = {
+	new_game : 0,
+	new_sprite : 0,
+	load_game : 0,
+	load_sprite : 0
+}
+
+var x_buttons = {
+	space : 0,
+	space_presed : false,
+	pause : 0,
+	pause_presed : false,
+	pause_down : false,
+	mini_map : 0,
+	mini_map_presed : false
+}
+
+var int=14;
+var string;
+
+var player = {
+	sprite : 0,
+	x : 0,
+	y : 0,
+	hp_max : 30,
+	hp : 30,
+	hp_re : 1,
+	hp_ui : 0,
+	ap_max : 20,
+	ap : 20,
+	ap_re : 1,
+	ap_ui : 0,
+}
+
+var move_rule = {
+	l : false,
+	r : false,
+	u : false,
+	d : false,
+	x_ass : false,
+	y_ass : false
+}
+
+var ui = {
+	pause : 0,
+	inventory : 0,
+	mini_map : 0
+}
+
+var map;
+
+var min_map;
+
+var waters=0;
+
+function preload() {
+	game.stage.backgroundColor = '#666666';
+
+
+	//Tiles
+	game.load.image('grass', 'assets/tiles/grass.png');  //grass
+	game.load.image('grass2', 'assets/tiles/grass2.png');  //grass2
+	game.load.image('grass3', 'assets/tiles/grass3.png');  //grass3
+	game.load.image('sand', 'assets/tiles/sand.png');  //sand
+	game.load.image('tree', 'assets/tiles/tree.png');  //tree
+	game.load.image('tree2', 'assets/tiles/tree2.png');  //tree2
+	game.load.image('water', 'assets/tiles/water.png'); //water
+	game.load.image('water2', 'assets/tiles/water2.png'); //water2
+	game.load.image('water3', 'assets/tiles/water3.png'); //water3
+
+	//Characters
+	game.load.spritesheet('man', 'assets/tiles/man.png', 48, 48, 4);  //sleep
+
+	//Buttond & ui
+	game.load.image('night', 'assets/ui/night.png'); //overlayer
+	game.load.image('pause', 'assets/ui/pause.png'); //pauselayer
+	game.load.image('new_button', 'assets/ui/buttons/new_button.png');  //new game
+	game.load.image('load_button', 'assets/ui/buttons/load_button.png'); //load game
+	game.load.image('HP', 'assets/ui/buttons/HP.png'); //hp ui
+	game.load.image('AP', 'assets/ui/buttons/AP.png'); //ap ui
+
+}
+
+function create() {
+
+	//string = ""+int;
+	//alert(string);
+	//alert(string-"");
+	boot.load_sprite = game.add.sprite(map_prop.game_x/2-104.5,map_prop.game_y/2-63,'load_button');
+	boot.new_sprite = game.add.sprite(map_prop.game_x/2-102,map_prop.game_y/2+10,'new_button');
+	//game.time.events.loop(Phaser.Timer.SECOND, setTime, this);
+
+}
+
+function setTime() {
+	if(turn.time>=230){
+		turn.pas*=-1;
+		//turn.time-=(turn.time-48);
+	}else if(turn.time<=-10){
+		turn.pas*=-1;
+		//turn.time-=(turn.time*2);
+	}
+	//alert(turn.time);
+	//alert(night.alpha);
+	night.alpha = 1*(turn.time/220);
+	if(night.alpha>1){
+		night.alpha = 1;
+	}else if(night.alpha<0){
+		night.alpha = 0;
+	}
+	//alert(night.alpha);
+}
+
+function update() {
+	x_buttons.pause = game.input.keyboard.addKey(Phaser.Keyboard.P);
+	x_buttons.mini_map = game.input.keyboard.addKey(Phaser.Keyboard.M);
+	if(x_buttons.pause_presed==true && x_buttons.pause_down==false){
+		if(x_buttons.pause.isDown && x_buttons.pause_presed==true){
+			x_buttons.pause_presed=false;
+			ui.pause.visible = false;
+			x_buttons.pause_down = true;
+			setTime();
+	    }else if(x_buttons.pause.isUp){
+	    	x_buttons.pause_down = false;
+	    }
+	}else if(boot.new_game==1){
+		
+		//Set world bounds and creates cursors
+		game.world.setBounds(0-(map_prop.game_x-map_prop.tile_size)/2, 0-(map_prop.game_y-map_prop.tile_size)/2, map_prop.width*map_prop.tile_size+(map_prop.game_x-map_prop.tile_size), map_prop.height*map_prop.tile_size+(map_prop.game_y-map_prop.tile_size));
+
+		//Generates a map
+		map=generate_map(map_prop.width,map_prop.height,true,true);
+		
+		//Draw all the map tiles
+		//map_prop.field = game.add.sprite(0, 0, 'grass3');
+		//map_prop.field.scale.setTo(map_prop.width, map_prop.height);
+		map=build_map(map_prop.cord_x,map_prop.cord_y);
+		spawnPlayer(0,0,'man',map_prop.cord_x,map_prop.cord_y);
+		min_map = game.add.group();
+		make_mini_map(1);
+		turn = {
+			time : rnd(0,200),
+			pas : 1
+		}
+		night = game.add.sprite(0, 0,'night');
+		night.scale.setTo(map_prop.width, map_prop.height);
+		night.fixedToCamera = true;
+		ui.pause = game.add.sprite(220, 0, 'pause');
+		ui.pause.fixedToCamera = true;
+		ui.pause.visible = false;
+		setTime();
+		game.camera.x = player.x*map_prop.tile_size-(map_prop.game_x-map_prop.tile_size)/2;
+		game.camera.y = player.y*map_prop.tile_size-(map_prop.game_y-map_prop.tile_size)/2;
+
+		boot.new_game=2;
+		
+	}else if(boot.load_game==1){
+
+	}else if(boot.new_game==2 || boot.load_game==2){
+		x_buttons.space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		var cursors = game.input.keyboard.createCursorKeys();
+		if (x_buttons.pause.isDown && x_buttons.pause_presed==false && x_buttons.pause_down==false){
+			x_buttons.pause_presed=true;
+			ui.pause.visible = true;
+			x_buttons.pause_down = true;
+			night.alpha=1;
+	    }else if(x_buttons.pause.isUp){
+	    	x_buttons.pause_down = false;
+	    }
+	    if (x_buttons.mini_map.isDown && x_buttons.mini_map_presed==false){
+			x_buttons.mini_map_presed=true;
+			make_mini_map(0);
+	    }else if(x_buttons.mini_map.isUp){
+	    	x_buttons.mini_map_presed=false;
+	    }
+		if (x_buttons.space.isDown && x_buttons.space_presed==false){
+	        x_buttons.space_presed=true;
+	        turn.time+=turn.pas;
+	        player.ap+=player.ap_re*1.5;
+	        if(player.ap>player.ap_max){
+	        	player.ap=player.ap_max;
+	        }
+			player.ap_ui.scale.setTo(player.ap/player.ap_max, 1);
+			setTime();
+	    }else if(x_buttons.space.isUp){
+	    	 x_buttons.space_presed=false;
+	    }
+		if (cursors.up.isDown && y_ass==false){
+	        if(player.y>map_prop.height*map_prop.cord_y && (map[player.y-1][player.x].id<1 || map[player.y-1][player.x].id==2)){
+	        	player.sprite.y -= map_prop.tile_size;
+	        	player.y--;
+	        	game.camera.y -= map_prop.tile_size;
+	        	turn.time+=turn.pas;
+	        	player.ap+=player.ap_re;
+		        if(player.ap>player.ap_max){
+		        	player.ap=player.ap_max;
+		        }
+		        player.ap_ui.scale.setTo(player.ap/player.ap_max, 1);
+		        setTime();
+	        }else if(player.y>0 && (map[player.y-1][player.x].id==2 || map[player.y-1][player.x].id<1)){
+	        	map=build_map(map_prop.cord_x,map_prop.cord_y-1);
+	        	player.sprite.destroy();
+	        	spawnPlayer(player.x,player.y-1,'man',map_prop.cord_x,map_prop.cord_y-1);
+	        	map_prop.cord_y--;
+	        	game.camera.x = (player.x-map_prop.width*map_prop.cord_x)*map_prop.tile_size-(map_prop.game_x-map_prop.tile_size)/2;
+				game.camera.y = (player.y+map_prop.height*map_prop.cord_y)*map_prop.tile_size-(map_prop.game_y-map_prop.tile_size)/2;
+	        	turn.time+=turn.pas;
+	        	player.ap+=player.ap_re;
+		        if(player.ap>player.ap_max){
+		        	player.ap=player.ap_max;
+		        }
+		        player.ap_ui.scale.setTo(player.ap/player.ap_max, 1);
+		        setTime();
+		        game.world.bringToTop(min_map);
+		        night.bringToTop(); 
+				ui.pause.bringToTop(); 
+	        }
+	        player.sprite.animations.play('up');
+	        move_rule.u=true;
+	        y_ass=true;
+	    }else if(cursors.up.isUp && move_rule.d==false){
+	    	 y_ass=false;
+	    	 move_rule.u=false;
+	    }
+	    if (cursors.down.isDown && y_ass==false){
+	        if(player.y<map_prop.height*(map_prop.cord_y+1)-1 && (map[player.y+1][player.x].id<1 || map[player.y+1][player.x].id==2)){
+	        	player.sprite.y += map_prop.tile_size;
+	        	player.y++;
+	        	game.camera.y += map_prop.tile_size;
+	        	turn.time+=turn.pas;
+	        	player.ap+=player.ap_re;
+		        if(player.ap>player.ap_max){
+		        	player.ap=player.ap_max;
+		        }
+		        player.ap_ui.scale.setTo(player.ap/player.ap_max, 1);
+		        setTime();
+	        }else if(player.y<map_prop.height*map_prop.world_size-1 && (map[player.y+1][player.x].id==2 || map[player.y+1][player.x].id<1)){
+	        	map=build_map(map_prop.cord_x,map_prop.cord_y+1);
+	        	player.sprite.destroy();
+	        	spawnPlayer(player.x,player.y+1,'man',map_prop.cord_x,map_prop.cord_y+1);
+	        	map_prop.cord_y++;
+	        	game.camera.x = (player.x-map_prop.width*map_prop.cord_x)*map_prop.tile_size-(map_prop.game_x-map_prop.tile_size)/2;
+				game.camera.y = (player.y-map_prop.height*map_prop.cord_y)*map_prop.tile_size-(map_prop.game_y-map_prop.tile_size)/2;
+	        	turn.time+=turn.pas;
+	        	player.ap+=player.ap_re;
+		        if(player.ap>player.ap_max){
+		        	player.ap=player.ap_max;
+		        }
+		        player.ap_ui.scale.setTo(player.ap/player.ap_max, 1);
+		        setTime();
+		        game.world.bringToTop(min_map);
+		        night.bringToTop(); 
+				ui.pause.bringToTop(); 
+	        }
+	        player.sprite.animations.play('down');
+	        move_rule.d=true;
+	        y_ass=true;
+	    }else if(cursors.down.isUp && move_rule.u==false){
+	    	 move_rule.d=false;
+	    	 y_ass=false;
+	    }
+	    if (cursors.left.isDown && x_ass==false){
+	        if(player.x>map_prop.width*map_prop.cord_x && (map[player.y][player.x-1].id<1 || map[player.y][player.x-1].id==2)){
+	        	player.sprite.x -= map_prop.tile_size;
+	        	player.x--;
+	        	game.camera.x -= map_prop.tile_size;
+	        	turn.time+=turn.pas;
+	        	player.ap+=player.ap_re;
+		        if(player.ap>player.ap_max){
+		        	player.ap=player.ap_max;
+		        }
+		        player.ap_ui.scale.setTo(player.ap/player.ap_max, 1);
+		        setTime();
+	        }else if(player.x>0 && (map[player.y][player.x-1].id==2 || map[player.y][player.x-1].id<1)){
+	        	map=build_map(map_prop.cord_x-1,map_prop.cord_y);
+	        	player.sprite.destroy();
+	        	spawnPlayer(player.x-1,player.y,'man',map_prop.cord_x-1,map_prop.cord_y);
+	        	map_prop.cord_x--;
+	        	game.camera.x = (player.x+map_prop.width*map_prop.cord_x)*map_prop.tile_size-(map_prop.game_x-map_prop.tile_size)/2;
+				game.camera.y = (player.y-map_prop.height*map_prop.cord_y)*map_prop.tile_size-(map_prop.game_y-map_prop.tile_size)/2;
+	        	turn.time+=turn.pas;
+	        	player.ap+=player.ap_re;
+		        if(player.ap>player.ap_max){
+		        	player.ap=player.ap_max;
+		        }
+		        player.ap_ui.scale.setTo(player.ap/player.ap_max, 1);
+		        setTime();
+		        game.world.bringToTop(min_map);
+		        night.bringToTop(); 
+				ui.pause.bringToTop(); 
+	        }
+	        player.sprite.animations.play('left');
+	        move_rule.l=true;
+	        x_ass=true;
+	    }else if(cursors.left.isUp && move_rule.r==false){
+	    	 move_rule.l=false;
+	    	 x_ass=false;
+	    }
+	    if (cursors.right.isDown && x_ass==false){
+	        if(player.x<map_prop.width*(map_prop.cord_x+1)-1 && (map[player.y][player.x+1].id<1 || map[player.y][player.x+1].id==2)){
+	        	game.camera.x += map_prop.tile_size;
+	        	player.sprite.x += map_prop.tile_size;
+	        	player.x++;
+	        	turn.time+=turn.pas;
+	        	player.ap+=player.ap_re;
+		        if(player.ap>player.ap_max){
+		        	player.ap=player.ap_max;
+		        }
+		        player.ap_ui.scale.setTo(player.ap/player.ap_max, 1);
+		        setTime();
+	        }else if(player.x<map_prop.width*map_prop.world_size-1 && (map[player.y][player.x+1].id==2 || map[player.y][player.x+1].id<1)){
+	        	map=build_map(map_prop.cord_x+1,map_prop.cord_y);
+	        	player.sprite.destroy();
+	        	spawnPlayer(player.x+1,player.y,'man',map_prop.cord_x+1,map_prop.cord_y);
+	        	map_prop.cord_x++;
+	        	game.camera.x = (player.x-map_prop.width*map_prop.cord_x)*map_prop.tile_size-(map_prop.game_x-map_prop.tile_size)/2;
+				game.camera.y = (player.y-map_prop.height*map_prop.cord_y)*map_prop.tile_size-(map_prop.game_y-map_prop.tile_size)/2;
+	        	turn.time+=turn.pas;
+	        	player.ap+=player.ap_re;
+		        if(player.ap>player.ap_max){
+		        	player.ap=player.ap_max;
+		        }
+		        player.ap_ui.scale.setTo(player.ap/player.ap_max, 1);
+		        setTime();
+		        game.world.bringToTop(min_map);
+		        night.bringToTop(); 
+				ui.pause.bringToTop(); 
+	        }
+	        player.sprite.animations.play('right');
+	        move_rule.r=true;
+	        x_ass=true;
+	    }else if(cursors.right.isUp && move_rule.l==false){
+	    	 move_rule.r=false;
+	    	 x_ass=false;
+	    }
+	}else{
+		var mouse = {
+			local : game.input.activePointer
+		}
+		if(localStorage.getItem('IsSaved')!=1){
+			boot.load_sprite.alpha=0.5;
+		}else if (mouse.local.x>=boot.load_sprite.x && mouse.local.x<=boot.load_sprite.x+209 && mouse.local.y>=boot.load_sprite.y && mouse.local.y<=boot.load_sprite.y+53){
+	        boot.load_sprite.alpha=1;
+	        if(mouse.local.isDown){
+	        	boot.load_game=1;
+	        	boot.load_sprite.destroy();
+	        	boot.new_sprite.destroy();
+	        }
+	    }else{
+	    	boot.load_sprite.alpha=0.8;
+	    }
+	    if (mouse.local.x>=boot.new_sprite.x && mouse.local.x<=boot.new_sprite.x+204 && mouse.local.y>=boot.new_sprite.y && mouse.local.y<=boot.new_sprite.y+53){
+	        boot.new_sprite.alpha=1;
+	        if(mouse.local.isDown){
+	        	boot.new_game=1;
+	        	boot.load_sprite.destroy();
+	        	boot.new_sprite.destroy();
+	        }
+	    }else{
+	    	boot.new_sprite.alpha=0.8;
+	    }
+	}
+
+}
+
+function rnd(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
---
+++ games/RPG_Game/js/map_gen.js
@@ -0,0 +1,472 @@
+//0-grass
+//1-tree
+//2-sand
+//3-water
+function gen_game(){
+
+	
+}
+
+function generate_map(width,height,gen_trees,gen_water){
+	var map1 = new Array();
+	for (i=0;i<height*map_prop.world_size;i++) {
+		map1[i]=new Array();
+		for (j=0;j<width*map_prop.world_size;j++) {
+			map1[i][j]= {
+				id : 0,
+				sprite_1 : 'NAV'
+			};
+		}
+	}
+
+	for (i=0;i<height*map_prop.world_size;i++) {
+		for (j=0;j<width*map_prop.world_size;j++) {
+			
+			if(gen_water==true){
+				var decision = rnd(0,250);
+
+				if(decision == 0 || (i==0 || i+1==height*map_prop.world_size || j==0 || j+1==width*map_prop.world_size)){
+					waters++;
+					var nr=1;
+					if(i<3 || i+3>height*map_prop.world_size || j<3 || j+3>width*map_prop.world_size){
+						nr=9;
+					}
+				//--------LAKE GENERATION--------------
+					var water_count = rnd(5,40);
+					// var water_count = 5;
+
+					map1[i][j].id=3;
+					make_beach(map1,i,j,height*map_prop.world_size,width*map_prop.world_size,nr);
+
+					var checked = new Array();
+
+					for (q=0;q<height*map_prop.world_size;q++) {
+						checked[q]=new Array();
+						for (w=0;w<width*map_prop.world_size;w++) {
+							checked[q][w]=0;
+						}
+					}
+
+					var starting = {
+						x : j,
+						y : i
+					};
+
+					var rinda = [];
+
+					rinda.push(starting);
+					checked[starting.y][starting.x] = 1;
+
+					// for(var q = 0;q<water_count;q++){
+					while(rinda.length > 0 && water_count>0){
+						var rnd_top = rnd(5,30);
+						var rnd_right = rnd(5,30);
+						var rnd_left = rnd(5,30);
+						var rnd_bottom = rnd(5,30);
+
+						var p = rinda.shift(); //Current water tile
+
+						//North flow
+						if(p.y > 0 && rnd_top>10 && (map1[p.y-1][p.x].id==0 || map1[p.y-1][p.x].id==2)){
+							checked[p.y-1][p.x]=1;
+							var c = {
+								x : p.x,
+								y : p.y-1
+							};
+							rinda.push(c);
+
+							map1[p.y-1][p.x].id=3;
+							make_beach(map1,p.y-1,p.x,height*map_prop.world_size,width*map_prop.world_size,nr);
+						}
+						//East flow
+						if(p.x < width*map_prop.world_size-1 && rnd_right>10 && (map1[p.y][p.x+1].id==0 || map1[p.y][p.x+1].id==2)){
+							checked[p.y][p.x+1]=1;
+							var c = {
+								x : p.x+1,
+								y : p.y
+							};
+							rinda.push(c);
+
+							map1[p.y][p.x+1].id=3;
+							make_beach(map1,p.y,p.x+1,height*map_prop.world_size,width*map_prop.world_size,nr);
+						}
+						//West flow
+						if(p.x > 0 && rnd_left>10 && (map1[p.y][p.x-1].id==0 || map1[p.y][p.x-1].id==2)){
+							checked[p.y][p.x-1]=1;
+							var c = {
+								x : p.x-1,
+								y : p.y
+							};
+							rinda.push(c);
+
+							map1[p.y][p.x-1].id=3;
+							make_beach(map1,p.y,p.x-1,height*map_prop.world_size,width*map_prop.world_size,nr);
+						}
+						//Down flow
+						if(p.y < height*map_prop.world_size-1 && rnd_left>10 && (map1[p.y+1][p.x].id==0 || map1[p.y+1][p.x].id==2)){
+							checked[p.y+1][p.x]=1;
+							var c = {
+								x : p.x,
+								y : p.y+1
+							};
+							rinda.push(c);
+
+							map1[p.y+1][p.x].id=3;
+							make_beach(map1,p.y+1,p.x,height*map_prop.world_size,width*map_prop.world_size,nr);
+						}
+						water_count--;
+					}
+					//---------------------------------------
+				}
+				
+			}
+
+			if(gen_trees==true){
+				var decision = rnd(0,25);
+
+				if(decision == 0){
+					//--------FOREST GENERATION--------------
+					var tree_count = rnd(5,100);
+					// var water_count = 5;
+					if(map1[i][j].nr<2){
+						map1[i][j].id=1;
+					}
+
+					var checked = new Array();
+
+					for (q=0;q<height*map_prop.world_size;q++) {
+						checked[q]=new Array();
+						for (w=0;w<width*map_prop.world_size;w++) {
+							checked[q][w]=0;
+						}
+					}
+
+					var starting = {
+						x : j,
+						y : i
+					};
+
+					var rinda = [];
+
+					rinda.push(starting);
+					checked[starting.y][starting.x] = 1;
+
+					// for(var q = 0;q<water_count;q++){
+					while(rinda.length > 0 && tree_count>0){
+						var rnd_top = rnd(0,2);
+						var rnd_right = rnd(0,2);
+						var rnd_left = rnd(0,2);
+						var rnd_bottom = rnd(0,2);
+
+						var p = rinda.shift(); //Current water tile
+
+						//North flow
+						if(p.y > 0 && rnd_top==2 && map1[p.y-1][p.x].id==0){
+							checked[p.y-1][p.x]=1;
+							var c = {
+								x : p.x,
+								y : p.y-1
+							};
+							rinda.push(c);
+
+							map1[p.y-1][p.x].id=1;
+						}
+						//East flow
+						if(p.x < width*map_prop.world_size-1 && rnd_right==2 && map1[p.y][p.x+1].id==0){
+							checked[p.y][p.x+1]=1;
+							var c = {
+								x : p.x+1,
+								y : p.y
+							};
+							rinda.push(c);
+							
+							map1[p.y][p.x+1].id=1;
+						}
+						//West flow
+						if(p.x > 0 && rnd_left==2 && map1[p.y][p.x-1].id==0){
+							checked[p.y][p.x-1]=1;
+							var c = {
+								x : p.x-1,
+								y : p.y
+							};
+							rinda.push(c);
+							
+							map1[p.y][p.x-1].id=1;
+						}
+						//Down flow
+						if(p.y < height*map_prop.world_size-1 && rnd_bottom==2 && map1[p.y+1][p.x].id==0){
+							checked[p.y+1][p.x]=1;
+							var c = {
+								x : p.x,
+								y : p.y+1
+							};
+							rinda.push(c);
+							
+							map1[p.y+1][p.x].id=1;
+						}
+						tree_count--;
+					}
+					//---------------------------------------
+				}
+			}
+
+		}
+	}
+
+
+	return map1;
+}
+
+function make_beach(m,y,x,height,width,nr){
+	var map2=m;
+	var sandy=0;
+	var a = {
+		x : x,
+		y : y
+	};
+	var rinda = [];
+	rinda.push(a);
+	while(rinda.length > 0 && sandy<nr){
+		var c = rinda.shift();
+		if(c.y>0){
+			if(map2[c.y-1][c.x].id<2){
+				map2[c.y-1][c.x].id=2;
+				a = {
+					x : c.x,
+					y : c.y-1
+				};
+				rinda.push(a);
+			}
+			if(c.x>0 && map2[c.y-1][c.x-1].id<2){
+				map2[c.y-1][c.x-1].id=2;
+				a = {
+					x : c.x-1,
+					y : c.y-1
+				};
+				rinda.push(a);
+			}
+			if(c.x<width-1 && map2[c.y-1][c.x+1].id<2){
+				map2[c.y-1][c.x+1].id=2;
+				a = {
+					x : c.x+1,
+					y : c.y-1
+				};
+				rinda.push(a);
+			}	
+		}
+		if(c.y<height-1){
+			if(map2[c.y+1][c.x].id<2){
+				map2[c.y+1][c.x].id=2;
+				a = {
+					x : c.x,
+					y : c.y+1
+				};
+				rinda.push(a);
+			}
+			if(c.x>0 && map2[c.y+1][c.x-1].id<2){
+				map2[c.y+1][c.x-1].id=2;
+				a = {
+					x : c.x-1,
+					y : c.y+1
+				};
+				rinda.push(a);
+			}
+			if(c.x<width-1 && map2[c.y+1][c.x+1].id<2){
+				map2[c.y+1][c.x+1].id=2;
+				a = {
+					x : c.x+1,
+					y : c.y+1
+				};
+				rinda.push(a);
+			}
+		}
+		if(c.x>0 && map2[c.y][c.x-1].id<2){
+			map2[c.y][c.x-1].id=2;
+			a = {
+				x : c.x-1,
+				y : c.y
+			};
+			rinda.push(a);
+		}
+		if(c.x<width-1 && map2[c.y][c.x+1].id<2){
+			map2[c.y][c.x+1].id=2;
+			a = {
+				x : c.x+1,
+				y : c.y
+			};
+			rinda.push(a);
+		}
+		sandy++;	
+	}
+}
+
+function build_map(cor_x,cor_y){
+	for(var i=map_prop.height*cor_y;i<map_prop.height+map_prop.height*cor_y;i++){
+		for(var j=map_prop.width*cor_x;j<map_prop.width+map_prop.width*cor_x;j++){
+			if(map_prop.cord_x>cor_x){
+				map[i][j+map_prop.width].sprite_1.destroy();
+			}else if(map_prop.cord_y>cor_y){
+				map[i+map_prop.height][j].sprite_1.destroy();
+			}else if(map_prop.cord_x<cor_x){
+				map[i][j-map_prop.width].sprite_1.destroy();
+			}else if(map_prop.cord_y<cor_y){
+				map[i-map_prop.height][j].sprite_1.destroy();
+			}
+			var nx = (j-map_prop.width*cor_x)*map_prop.tile_size;
+			var ny = (i-map_prop.height*cor_y)*map_prop.tile_size;
+			if(map[i][j].id==0){
+				var r = rnd(1,7);
+				if(r<=4){
+					map[i][j].sprite_1 = game.add.sprite(nx, ny, 'grass');
+				}else if(r<=6){
+					map[i][j].id = 0.1;
+					map[i][j].sprite_1 = game.add.sprite(nx, ny, 'grass2');
+				}else{
+					map[i][j].id = 0.2;
+					map[i][j].sprite_1 = game.add.sprite(nx, ny, 'grass3');
+				}
+			}else if(map[i][j].id==1){
+				map[i][j].sprite_1 = game.add.sprite(nx, ny, 'grass');
+				var r = rnd(1,4);
+				if(r>1){
+					map[i][j].sprite_1 = game.add.sprite(nx, ny, 'tree');
+				}else{
+					map[i][j].sprite_1 = game.add.sprite(nx, ny, 'tree2');
+					map[i][j].id = 1.1;
+				}
+			}else if(map[i][j].id==2){
+				map[i][j].sprite_1=game.add.sprite(nx, ny, 'sand');
+			}else if(map[i][j].id==3){
+				var r = rnd(1,3);
+				if(r==1){
+					map[i][j].sprite_1=game.add.sprite(nx, ny, 'water');
+				}else if(r==2){
+					map[i][j].id = 3.1;
+					map[i][j].sprite_1 = game.add.sprite(nx, ny, 'water2');
+				}else{
+					map[i][j].id = 3.2;
+					map[i][j].sprite_1 = game.add.sprite(nx, ny, 'water3');
+				}
+			}
+		}
+	}
+	return map;
+}
+
+function make_mini_map(dose){
+	if(dose==1 && map_prop.min_map==true){
+		var sc=(map_prop.game_y/5*2)/(map_prop.tile_size*(map_prop.height*map_prop.world_size));
+		alert(sc);
+		for(var i=0;i<map_prop.height*map_prop.world_size;i++){
+			for(var j=0;j<map_prop.width*map_prop.world_size;j++){
+				var nx = j*(map_prop.tile_size*sc);
+				var ny = i*(map_prop.tile_size*sc)+map_prop.game_y/5*3;
+				if(map[i][j].id==0){
+					var main = game.add.sprite(nx, ny, 'grass');
+					main.scale.setTo(sc,sc);
+					main.alpha=0.5;
+					main.fixedToCamera=true;
+					min_map.add(main);
+				}if(map[i][j].id==0.1){
+					var main = game.add.sprite(nx, ny, 'grass2');
+					main.scale.setTo(sc,sc);
+					main.alpha=0.5;
+					main.fixedToCamera=true;
+					min_map.add(main);
+				}if(map[i][j].id==0.2){
+					var main = game.add.sprite(nx, ny, 'grass3');
+					main.scale.setTo(sc,sc);
+					main.alpha=0.5;
+					main.fixedToCamera=true;
+					min_map.add(main);
+				}else if(map[i][j].id==1){
+					var main = game.add.sprite(nx, ny, 'grass3');
+					main.scale.setTo(sc,sc);
+					main.alpha=0.5;
+					main.fixedToCamera=true;
+					min_map.add(main);
+					var main = game.add.sprite(nx, ny, 'tree');
+					main.scale.setTo(sc,sc);
+					main.alpha=0.5;
+					main.fixedToCamera=true;
+					min_map.add(main);
+				}else if(map[i][j].id==1.1){
+					var main = game.add.sprite(nx, ny, 'grass3');
+					main.scale.setTo(sc,sc);
+					main.alpha=0.5;
+					main.fixedToCamera=true;
+					min_map.add(main);
+					var main = game.add.sprite(nx, ny, 'tree2');
+					main.scale.setTo(sc,sc);
+					main.alpha=0.5;
+					main.fixedToCamera=true;
+					min_map.add(main);
+				}else if(map[i][j].id==2){
+					var main = game.add.sprite(nx, ny, 'sand');
+					main.scale.setTo(sc,sc);
+					main.alpha=0.5;
+					main.fixedToCamera=true;
+					min_map.add(main);
+				}else if(map[i][j].id==3){
+					var main = game.add.sprite(nx, ny, 'water');
+					main.scale.setTo(sc,sc);
+					main.alpha=0.5;
+					main.fixedToCamera=true;
+					min_map.add(main);
+				}else if(map[i][j].id==3.1){
+					var main = game.add.sprite(nx, ny, 'water2');
+					main.scale.setTo(sc,sc);
+					main.alpha=0.5;
+					main.fixedToCamera=true;
+					min_map.add(main);
+				}else if(map[i][j].id==3.2){
+					var main = game.add.sprite(nx, ny, 'water3');
+					main.scale.setTo(sc,sc);
+					main.alpha=0.5;
+					main.fixedToCamera=true;
+					min_map.add(main);
+				}
+			}
+		}
+	}else if(map_prop.min_map==true){
+		min_map.destroy();
+		min_map = game.add.group();
+		make_mini_map(1);
+		night.bringToTop(); 
+		ui.pause.bringToTop(); 
+	}
+}
+
+function spawnPlayer(x,y,name,cor_x,cor_y){
+	var tpx = x;
+	var tpy = y;
+	while(tpy>=0){
+		if(map[tpy][tpx].id==0 || map[tpy][tpx].id==2){
+			var gtpx = (tpx-map_prop.width*cor_x)*map_prop.tile_size;
+			var gtpy = (tpy-map_prop.height*cor_y)*map_prop.tile_size;
+			player.sprite = game.add.sprite(gtpx, gtpy, name);
+			player.x=tpx;
+			player.y=tpy;
+			player.hp_ui = game.add.sprite(10,10,'HP');
+			player.hp_ui.fixedToCamera=true;
+			player.ap_ui = game.add.sprite(10,40,'AP');
+			player.ap_ui.fixedToCamera=true;
+			player.sprite.animations.add('up', [0], 10, true);
+			player.sprite.animations.add('right', [1], 10, true);
+			player.sprite.animations.add('down', [2], 10, true);
+			player.sprite.animations.add('left', [3], 10, true);
+			break;
+		}else if(cor_x!=map_prop.cord_x || cor_x!=map_prop.cord_x){
+			break;
+		}
+		tpx = rnd(map_prop.width*cor_x,map_prop.width-2+map_prop.width*cor_x);
+		tpy = rnd(map_prop.height*cor_y,map_prop.height-2+map_prop.height*cor_y);
+	}
+
+}
+
+function rnd(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+
