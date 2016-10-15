488124005a617faa2503669147ac76e1f9b37c91
Emīls <emilsromanis@gmail.com>
2016-10-15 21:10:12 +0300

    Update work.js

--- games/RPG_Game/js/work.js
+++ games/RPG_Game/js/work.js
@@ -1,12 +1,30 @@
 function cut_tree(x,y,cor_x,cor_y){
 	for (var i = 0; i < this_map.children.length; i++) {
-		if(this_map.children[i].key=='tree' || this_map.children[i].key=='tree2'){
+		if(this_map.children[i].key=='tree'){
 			if(this_map.children[i].x==x && this_map.children[i].y==y){
 				this_map.children[i].destroy();
-				inventory.log++;
+				inventory.log+=rnd(0,3);
 				inventory.logText.text = inventory.log;
+				break;
+			}
+		}else if(this_map.children[i].key=='tree3'){
+			if(this_map.children[i].x==x && this_map.children[i].y==y){
+				this_map.children[i].destroy();
+				inventory.log+=0.5;
+				inventory.logText.text = inventory.log;
+				break;
+			}
+		}else if(this_map.children[i].key=='tree2'){
+			if(this_map.children[i].x==x && this_map.children[i].y==y){
+				this_map.children[i].destroy();
+				inventory.apple+=rnd(0,3);
+				inventory.appleText.text = inventory.apple;
 				break;
 			}
 		}
 	}
+}
+
+function rnd(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
 }
